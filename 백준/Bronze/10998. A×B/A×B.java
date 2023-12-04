d5a5e1126a1d4f5df507e1dacd7f299a527c1840
coffeeful <152365985+coffeeful@users.noreply.github.com>
2023-12-04 16:34:20 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        System.out.print("");
+        int num2 = scanner.nextInt();
+        System.out.print("");
+        int num3= num1 * num2;
+        System.out.println(num3);
+    }
+
+}
