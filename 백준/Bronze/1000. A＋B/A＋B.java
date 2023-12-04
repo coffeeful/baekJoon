6d97857e9a08ecce0f961070f0e8898d3e86d92d
coffeeful <152365985+coffeeful@users.noreply.github.com>
2023-12-04 16:28:00 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num1 = 0, num2 = 0;
+		num1 = scanner.nextInt();
+		num2 = scanner.nextInt();
+		int num3 = (num1+ num2);
+		
+		System.out.print(num3);
+		
+		
+		
+	}
+	
+}
