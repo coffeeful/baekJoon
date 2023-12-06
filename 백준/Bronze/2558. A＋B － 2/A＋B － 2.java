27b67d57106ccbee7003c591ae60b700c6b69036
coffeeful <152365985+coffeeful@users.noreply.github.com>
2023-12-06 16:49:51 +0900

    [Bronze V] Title: A+B - 2, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num1 = 0, num2 =0;
+
+
+        num1= scanner.nextInt();
+        num2= scanner.nextInt();
+        int sum = (num1+ num2);
+        System.out.println( sum);
+
+
+    }
+}
