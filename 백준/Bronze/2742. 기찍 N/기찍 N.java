0b1cd0c862dba54237a0c748942e49dfd381fd4c
coffeeful <152365985+coffeeful@users.noreply.github.com>
2023-12-08 12:57:53 +0900

    [Bronze IV] Title: 기찍 N, Time: 1092 ms, Memory: 31400 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int cnt =0;
+        cnt = scanner.nextInt();
+        for (int i =cnt; i >=1; i--){
+            System.out.println(i);
+        }
+    }
+}
