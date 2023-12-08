74948d88671cf155a797bcfa59baa38637903f3e
coffeeful <152365985+coffeeful@users.noreply.github.com>
2023-12-08 11:54:47 +0900

    [Bronze V] Title: N 찍기, Time: 1032 ms, Memory: 31964 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count =0;
+        count = scanner.nextInt();
+        int num =0;
+        for(int i =1;  i<=count;  i++){
+
+
+
+
+
+            System.out.println(i);
+
+
+        }
+    }
+}
